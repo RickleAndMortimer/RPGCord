2677b83f30d6880378b48e9e1fee2389640fa94f
Your Name <nathacutlan@gmail.com>
2020-09-12 20:56:29 -0400

    Create campaign.py

---
+++ campaign.py
@@ -0,0 +1,37 @@
+from discord import extentions.Commands
+import discord
+
+adventurers = list()
+
+description = "The kingdom had begun to fail. The towers were crumbling little by little, the castle had lost its regal glow, and the royal guard’s numbers had begun to drop rapidly. "
+                  +"Because of this, the royal family took it upon themselves to go to the village that they’d ruled, and find those worthy to help repair what had begun to become known as “The Broken Kingdom.”
+                  +"The king has disguised himself and seems no more than but a simple villager, but he seeks blacksmiths, archers, mages, and swordsmen."
+
+party = list()
+#add events to the story here
+event_text.append("")
+event_text.append ("")
+#add decisions to the story here
+
+
+#PART ONE
+"The king, with his you all by his side, embarks onto the Forgotten Kingdom. You traverse a forset and find a small shack housing an old hermit. The hermit leaps forward to the party asking for some Bitcoin. " +
+"Will your party accept?"
+
+"1. Yes 2. No"
+#get poll information from the discord messages
+poll = {vote1, vote2, vote3, vote4}
+if poll.index(max(poll)) == 0:
+    a
+    random.choice(adventurers).name
+    "The old man says thank you, but one of your party members finds themselves feeling ill. -15 to " + random.choice(adventurers).name
+    
+else if poll.index(max(poll)) == 1:
+    b
+    "The old man "
+#PART TWO
+
+
+def makeDecision():
+    
+
